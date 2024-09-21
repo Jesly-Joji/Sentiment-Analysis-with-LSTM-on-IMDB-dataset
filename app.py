c42f28e70a8a57a0162998f5c904f9d9dfa3cb82
Jesly Joji <133549580+Jesly-Joji@users.noreply.github.com>
2024-09-21 10:03:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+#IMPORT STATEMENTS
 import torch
+import requests
+from flask import Flask
+
+#Get the model from github
+url="https://raw.githubusercontent.com/Jesly-Joji/Sentiment-Analysis-with-LSTM-on-IMDB-dataset/main/sentiment_analysis_lstm.pt"
+
+response=request.get(url)
+
+with open("model.pt","wb") as file:
+  file.write(response.context)
+
+#Load the model
+model=torch.load("model.pt")
+
+  
